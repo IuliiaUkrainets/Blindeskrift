7e2ebedf13ce90087a37a7dc6610e59634615a7f
Iuliia Romanivna Ukrainets <iuk001@post.uit.no>
2018-11-18 11:34:48 +0100

    add test for table filter

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,48 @@
 package com.company;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
 
-    public static void main(String[] args) {
+    static Map<String, Integer> map = new HashMap<>();
+    static {
+        map.put("but", 10243);
+        map.put("can", 10249);
+        map.put("do", 10265);
+        map.put("every", 10257);
+        map.put("from", 10251);
+        map.put("go", 10267);
+        map.put("have", 10259);
+        map.put("just", 10266);
+        map.put("knowledge", 10280);
+        map.put("like", 10296);
+        map.put("more", 10253);
+        map.put("not", 10269);
+        map.put("people", 10255);
+        map.put("quite", 10271);
+        map.put("rather", 10263);
+        map.put("so", 10254);
+        map.put("that", 10270);
+        map.put("us", 10277);
+        map.put("very", 10279);
+        map.put("it", 10285);
+        map.put("you", 10301);
+        map.put("as", 10293);
+        map.put("and", 10287);
+        map.put("for", 10303);
+        map.put("of", 102295);
+        map.put("the", 10286);
+        map.put("with", 10302);
+        map.put("will", 10298);
+        map.put("his", 10278);
+        map.put("in", 10260);
+        map.put("was", 10292);
+        map.put("to", 10262);
+    }
 
+    public static void main(String[] args) {
+        System.out.println(new Character((char) 10286));
     }
 
     public String readInputFile(String path) {
@@ -12,5 +51,9 @@ public class Main {
 
     public String writeOutputFile(String path) {
         return null;
+    }
+
+    public static String filter(String text) {
+        return text;
     }
 }
--- src/com/company/MainTest.java
+++ src/com/company/MainTest.java
@@ -3,6 +3,8 @@ package com.company;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.*;
 
 public class MainTest {
@@ -11,10 +13,21 @@ public class MainTest {
     public void test(){
         Main main = mock(Main.class);
 
-        // define return value for method getUniqueId()
         when(main.readInputFile("input.txt")).thenReturn("Hello");
 
         assertEquals(main.readInputFile("input.txt"), "Hello");
     }
 
+    @Test
+    public void testFilter(){
+        Main main = mock(Main.class);
+
+        when(main.readInputFile("input.txt")).thenReturn("the end!");
+
+        String text = main.readInputFile("input.txt");
+        String filtered = Main.filter(text);
+        for (String value : Main.map.keySet()) {
+            assertFalse(filtered.contains(value));
+        }
+    }
 }
